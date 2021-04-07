f624685963153c33c6b4581213a9b3c11ed67528
Slugi <slugii@hotmail.com>
2021-04-07 22:59:47 +0200

    spring security register(token confimration not working properly yet)

---
+++ src/main/java/pl/diabeticjournal/controllers/WelcomeController.java
@@ -0,0 +1,24 @@
+package pl.diabeticjournal.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class WelcomeController {
+
+
+  @GetMapping("/hello")
+  public String hello(Principal principal, Model model) {
+    model.addAttribute("name", principal.getName());
+    return "Welcome";
+  }
+
+  @GetMapping("/admin")
+  public String forAdmin() {
+    return "hello-admin";
+  }
+}
